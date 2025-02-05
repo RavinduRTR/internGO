7467dee506e2b6519b977ccf16c378c636268404
Ravindu Theja <rtrravindu@gmail.com>
2025-02-05 13:20:06 +0530

    After Download FileZilla

--- header.php
+++ header.php
@@ -4,16 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 
     <header>
         <nav>
+            <div class="logo">
+                <img src="images/logo.png" alt="logo">
+            </div>
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#contact">Contact</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><img src="images/user-icon.png" alt="user-login" class="user-icon"></li>
             </ul>
         </nav>
     </header>
